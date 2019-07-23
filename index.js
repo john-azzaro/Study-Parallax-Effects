41c400185369c21564b2a0bad4598818a07f1ec7
John Azzaro <jazzaro@temple.edu>
2019-07-23 15:16:21 -0700

    updated js

--- index.js
+++ index.js
@@ -5,10 +5,21 @@ function parallax(element, distance, speed ) {                             // pa
     item.style.transform = `translateY(${distance * speed}px)`;            // when the parallax function is called, the item style will transform with "translateY" 
 }                                                                          // ... and "distance * speed" allows us to customize the parallax speed and distance.
 
+function handleScroll() {                                                  // when handleScroll is run...
+    $(document).on('scroll', function() {                                  // Within the document, when the user scolls the page, run the following:
+        parallax('header', window.scrollY, 0.7);                            // ... which calls the parallax function with three arguments passed to parallax (i.e. 'header' = element, window.scollY (i.e. the amount scrolled on the window) = distance, 1 = speed)
+        parallax('.kiwi', window.scrollY, 0.2);                                 
+        parallax('.apple', window.scrollY, 0.4);                                 
+        parallax('.mango', window.scrollY, 0.6);  
+    });
+}
+ 
+function setUpEventListeners() {                                            // run the event listener handleScroll.
+    handleScroll();
+}
 
-window.addEventListener('scroll', function() {                             // a listener will watch for "scroll", and when scrolling occurs, run the anonymous function..
-    parallax('header', window.scrollY, 0.7);                                 // ... which calls the parallax function with three arguments passed to parallax (i.e. 'header' = element, window.scollY (i.e. the amount scrolled on the window) = distance, 1 = speed)
-    parallax('.kiwi', window.scrollY, 0.2);                                 
-    parallax('.apple', window.scrollY, 0.4);                                 
-    parallax('.mango', window.scrollY, 0.6);                                     
-});
+function initialize() {                                                     // When the app is initialized, run setUpEventListeners.
+    setUpEventListeners();
+}
+
+$(initialize);                                                              // intialize the app.
