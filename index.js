356f3e005e03526dfff3efe74b4e22a9c5d76510
John Azzaro <jazzaro@temple.edu>
2019-07-23 15:10:05 -0700

    additional js modifications

--- index.js
+++ index.js
@@ -10,6 +10,5 @@ window.addEventListener('scroll', function() {                             // a 
     parallax('header', window.scrollY, 0.7);                                 // ... which calls the parallax function with three arguments passed to parallax (i.e. 'header' = element, window.scollY (i.e. the amount scrolled on the window) = distance, 1 = speed)
     parallax('.kiwi', window.scrollY, 0.2);                                 
     parallax('.apple', window.scrollY, 0.4);                                 
-    parallax('.mango', window.scrollY, 0.6);                                 
-    
-})
+    parallax('.mango', window.scrollY, 0.6);                                     
+});
