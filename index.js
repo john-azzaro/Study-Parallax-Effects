65f76136d70c16c92821cf0389f818ae5587b985
John Azzaro <jazzaro@temple.edu>
2019-07-23 13:28:50 -0700

    parallax selector issue

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict';
 
-function parallax() {
-    
+function parallax(element, distance, speed ) {                             // parallax function that takes 3 parameters: element, distance, and 
+    const item = document.querySelector(element);                          // the selected element is stored as "item".
+    item.style.transform = `translateY(${distance * speed}px)`             // when
 }
